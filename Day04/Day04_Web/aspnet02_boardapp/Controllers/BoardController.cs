b3b3b45b5a05de6b7da6b75a1fe5ba4a02a85f7f
kimjihyeon-angela <angela9830@naver.com>
2023-05-22 17:27:55 +0900

    2023.05.22.

---
+++ Day04/Day04_Web/aspnet02_boardapp/Controllers/BoardController.cs
@@ -0,0 +1,44 @@
+﻿using aspnet02_boardapp.Data;
+using aspnet02_boardapp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet02_boardapp.Controllers
+{
+    public class BoardController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public BoardController(ApplicationDbContext db)
+        {
+            _db = db; // 알아서 DB가 연결됨
+        }
+
+        // 게시판 최초화면 리스트
+        public IActionResult Index()
+        {
+            IEnumerable<Board> objBoardList = _db.Boards.ToList(); // SELECT 쿼리
+            return View(objBoardList);
+        }
+
+        // https://localhost:7177/Board/Create
+        // GetMethod로 화면을 URL로 부를때 쓰는 액션
+
+        // 게시판 글쓰기
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // submit이 발생해서 내부로 데이터를 전달하는 액션
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Board board)
+        {
+            _db.Boards.Add(board); // INSERT
+            _db.SaveChanges(); // COMMIT
+
+            return RedirectToAction("Index", "Board");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -6,4 +6,9 @@ ASP.NET 학습 리포지토리
 	- HTTP
 	- 웹 기본 구조(HTML5, CSS3, javascript)
 	- HTML5 기본
-	- 
+	
+	
+## Day 04
+- ASP.Net MVC
+	- ASP.Net 기본
+	- ASP.Net 프로젝트
