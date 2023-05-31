41c588717a1655fbff225bc24795b2ee42d82a47
kimjihyeon-angela <angela9830@naver.com>
2023-05-31 12:27:53 +0900

    2023.05.31.

---
+++ Day10/TodoItemSolution/TodoApiServer/Models/TodoItem.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TodoApiServer.Models
+{
+    public class TodoItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column("Title", TypeName = "Varchar(100)")]
+        public string? Title { get; set; }
+
+
+        public DateTime? TodoDate { get; set; }
+
+
+        public bool? IsCompleted { get; set; }
+    }
+}
